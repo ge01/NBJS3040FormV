47797d44d4cec7f264195d334f715f3942b501a3
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-03 14:11:54 -0500

    accessing form elements

---
+++ app.js
@@ -0,0 +1,10 @@
+/****************************************************
+*              Accessing Form Elements              *
+****************************************************/
+function validator(){
+  if(document.buckysForm.thebox.checked){
+    alert("Yes its checked!");
+  }else {
+    alert("no its not checked!");
+  }
+}
